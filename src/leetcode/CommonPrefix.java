fc937d8ec0ac46901da438d07813ea04f50863e8
Guilherme Felix <f3lixguilherme@gmail.com>
2025-09-09 22:58:48 -0300

    exercise 14 - quase certo eu espero

--- src/leetcode/CommonPrefix.java
+++ src/leetcode/CommonPrefix.java
@@ -2,7 +2,7 @@ package leetcode;
 
 public class CommonPrefix {
   public static void main(String[] args) {
-    String[] strs = { "ab", "ab" };
+    String[] strs = { "abab", "aba", "" };
     System.out.println(solution(strs));
   }
 
@@ -12,32 +12,29 @@ public class CommonPrefix {
       return strs[0];
     }
 
-    String prefix = "";
-    String firstWord = strs[0];
+    String prefix = strs[0];
     String nextWord;
     int i;
     int letra = 0;
     boolean isPrefix;
-    String prefixTemp = strs[0];
 
     for (i = 1; i < strs.length; i++) {
       nextWord = strs[i];
       letra = 0;
       isPrefix = true;
-
       while (isPrefix && letra < prefix.length()) {
 
-        if (letra >= firstWord.length() || letra > nextWord.length()) {
-          return prefix;
-        }
-        if (prefixTemp.charAt(letra) == nextWord.charAt(letra)) {
+        if (prefix.charAt(letra) == nextWord.charAt(letra)) {
           letra++;
-          prefixTemp = nextWord.substring(0, letra);
         } else {
           isPrefix = false;
+          prefix = nextWord.substring(0, letra);
           break;
         }
-        prefix = prefixTemp;
+        if (letra >= prefix.length() || letra >= nextWord.length()) {
+          prefix = nextWord.substring(0, letra);
+          break;
+        }
 
       }
     }
